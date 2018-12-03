f23701413ce7fc6201ed7abda7de44632d8097fb
moda707 <moh.dastpak@gmail.com>
2018-12-02 22:47:19 -0500

    12/2

--- SafarApp/SafarApp/MainPage.xaml.cs
+++ SafarApp/SafarApp/MainPage.xaml.cs
@@ -14,20 +14,7 @@ namespace SafarApp
         public MainPage()
         {
             InitializeComponent();
-
-            PusherOptions options = new PusherOptions();
-            options.Cluster = "us2";
-            Pusher pusher = new Pusher("650608", "87da8c910fa9a303c2f4", "be9679542369b5035426", options);
             
-            ChannelsList channel = pusher.subscribe("my-channel");
-
-            channel.bind("my-event", new SubscriptionEventListener() {
-                @Override
-                public void onEvent(String channelName, String eventName, final String data)
-                {
-                System.out.println(data);
-            }
-            });
         }
     }
 }
